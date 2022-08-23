ff29740f97218ae34d72e77768ff2dd2dc31bf05
Viktoriia <vikakovai19@yahoo.com>
2022-08-23 11:33:09 -0400

    added sout

--- src/first/HelloWorld/HelloWorld.java
+++ src/first/HelloWorld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //did a changes
+        System.out.println("Maybe 5 weeks of java left");
     }
 }
